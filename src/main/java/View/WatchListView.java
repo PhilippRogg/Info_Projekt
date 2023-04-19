e3058725f6c1fbc1a1ae21bee03509832db33b69
MyUsernameDDM <mayrdamian9@gmail.com>
2023-04-19 09:30:57 +0200

    WatchListView

--- src/main/java/View/WatchListView.java
+++ src/main/java/View/WatchListView.java
@@ -1,4 +1,36 @@
 package View;
 
+import javafx.scene.control.Button;
+
+import java.nio.Buffer;
+import java.util.ArrayList;
+
 public class WatchListView {
+    int articleAmount;
+    ArrayList<Button> buttonList = new ArrayList<>();
+
+    public WatchListView(int articleAmount) {
+        this.articleAmount = articleAmount;
+    }
+
+    /**
+     * Methode zum setzen der Artikel-Namen in der buttonList
+     * @param articleNames Enthält die Namen von den Artikeln die angezeigt werden sollen
+     *
+     */
+    public void setArticles(ArrayList<String> articleNames){
+        for (String str :articleNames) {
+            buttonList.add(new Button(str));
+        }
+    }
+    public void addArticle(String articleName){
+        buttonList.add(new Button(articleName));
+    }
+    public void removeArticle(String articleName){
+        for (Button b :buttonList) {
+            if(b.getText().equals(articleName)){
+                buttonList.remove(b);
+            }
+        }
+    }
 }
